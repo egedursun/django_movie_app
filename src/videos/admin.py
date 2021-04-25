6c6b540a38ad636504a96b30c14e6c4dcf3de549
Ege Dursun <edogandursun@gmail.com>
2021-04-26 01:36:07 +0300

    primitive admin features related to videos has been coded, video models have been created and tested

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import VideoPublishedProxy
+from .models import VideoAllProxy
+
+
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', 'video_id', 'is_published']
+    search_fields = ['title']
+    list_filter = ['active']
+    readonly_fields = ['id', 'is_published']
+    class Meta:
+        model = VideoAllProxy
+
+    #implement it in the model
+    """
+    def published(self, obj, *args, **kwargs):
+        return obj.active
+    """
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+    #list_filter = ['video_id']
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(active=True)
+
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
--- src/videos/models.py
+++ src/videos/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True) #Video
+    video_id = models.CharField(max_length=256)
+    active = models.BooleanField(default=True)
+    # timestamp
+    # updated
+    # state
+    # publish_timestamp
+
+    @property
+    def is_published(self):
+        return self.active
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+class VideoPublishedProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
