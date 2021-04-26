48eac5ddac2402242a7fb300ae5fcdfecd523a17
Ege Dursun <edogandursun@gmail.com>
2021-04-26 16:33:49 +0300

    Timestamp and state features are added to Videos

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -6,10 +6,10 @@ from .models import VideoAllProxy
 
 
 class VideoAllAdmin(admin.ModelAdmin):
-    list_display = ['title', 'id', 'video_id', 'is_published']
+    list_display = ['title', 'id', 'state', 'video_id', 'is_published']
     search_fields = ['title']
-    list_filter = ['active']
-    readonly_fields = ['id', 'is_published']
+    list_filter = ['state', 'active']
+    readonly_fields = ['id', 'is_published', 'publish_timestamp']
     class Meta:
         model = VideoAllProxy
 
